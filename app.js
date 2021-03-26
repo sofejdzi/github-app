a967ac67f9c15dbe57f159533fc217c7aaf31653
sofejdzi <sofijastolic@gmail.com>
2021-03-26 12:33:17 +0100

    Passing data into function and

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ gitHubForm.addEventListener('submit', (e) => {
 
     requestUserRepos(gitHubUsername); //pokretanje funkcije
 });
+
+function requestUserRepos(username){
+
+    const xhr = new XMLHttpRequest(); //novi objekat
+    const url = `https://api.github.com/users/${username}/repos`; //github endpoint
+
+    xhr.open('GET', url, true); //nova konekcija koristeci GET
+
+    xhr.onload=function(){
+
+        const data = JSON.parse(this.response); //parsovanje podataka u JSON
+
+        for (let d in data){
+
+            let ul = document.getElementById('userRepos');
+
+            let li = document.createElement('li');
+
+            li.classList.add('list-group-item') //dodavanje bootstrap klase za grupisanje liste
+
+            li.innerHTML = (`
+                <p><strong>Repo:</strong> ${data[d].name}</p>
+                <p><strong>Description:</strong> ${data[d].description}</p>
+                <p><strong>URL:</strong> <a href="${data[d].html_url}">${data[d].html_url}</a></p>
+            `);
+
+            ul.appendChild(li); //dodaje listi element
+        }
+    }
+    xhr.send();
+}
